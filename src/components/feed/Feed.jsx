259b34c506a4e523a7b9641af5046eff5d29c93b
Vishwajith-Shettigar <zekromvishwa56789@gmail.com>
2024-07-08 20:07:47 +0530

    make production ready

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,14 +4,13 @@ import Share from '../share/Share'
 import "./feed.css"
 import  axios  from 'axios'
 import { AuthContext } from '../../context/AuthContext'
-// import {Posts} from "../../dummyData"
 function Feed({username}) {
 
   const [posts,setPosts]=useState();
 const {user}=useContext(AuthContext);
 
   useEffect( ()=>{
-    
+  
  const fetchPosts=async()=>{
  console.log()
   const res=  username
